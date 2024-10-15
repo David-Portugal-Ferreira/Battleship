3cb322489a7c51c7b55c61f4ea97e9f0eadbe46b
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-10-15 17:18:41 +0100

    Place each player ship's at predefined locations. Add two boards to the html and some style to the boards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import { Ship } from "./modules/Ship"
+import { Player } from "./modules/Player";
+import "./index.css"
+
+const player1 = new Player("real");
+Object.keys(player1.gameboard.ships).forEach((ship, index) => {
+  player1.gameboard.placeShip(
+    player1.gameboard.ships[ship],
+    index,
+    0,
+    index,
+    0 + player1.gameboard.ships[ship].length,
+  );
+});
+console.log(player1.gameboard.board);
+
+const player2 = new Player("comp");
+Object.keys(player2.gameboard.ships).forEach((ship, index) => {
+  player2.gameboard.placeShip(
+    player2.gameboard.ships[ship],
+    index,
+    0,
+    index,
+    0 + player2.gameboard.ships[ship].length,
+  );
+});
+console.log(player2.gameboard.board);
---
+++ src/modules/dom-manipulation.js
@@ -0,0 +1,1 @@
+const myGameboard = document.querySelector(".my-gameboard");
